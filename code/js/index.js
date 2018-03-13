3da14f20480e7155c7cb43a5af620d95f4d12afd
aongi <aongi@github.com>
2018-03-13 23:08:33 +0800

    tijiao12

---
+++ code/js/index.js
@@ -0,0 +1,178 @@
+﻿function isMobile(e) {
+	var t = /^1[3|4|5|6|7|8|9][0-9]{9}$/;
+	if (t.test(e)) {
+		return !0
+	} else {
+		return !1
+	}
+};
+
+function andOrIos() {
+	var e = navigator.userAgent,
+		t = e.indexOf('Android') > -1 || e.indexOf('Adr') > -1,
+		i = !! e.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/);
+	if (t) {
+		return 'android'
+	};
+	if (i) {
+		return 'ios'
+	}
+};
+
+function isPassword(e) {
+	var t = /^(?![0-9]+$)(?![a-zA-Z]+$)(?![_\!@#\$%\^&\*\(\)_\+=|<>,\.{}:;\]\[\~\/\?"'\\-]+$)[A-Za-z0-9_\!@#\$%\^&\*\(\)_\+=|<>,\.{}:;\]\[\~\/\?"'\\-]{8,16}$/;
+	if (t.test(e)) {
+		return !0
+	} else {
+		return !1
+	}
+};
+
+
+
+$('#appLogin').click(function() {
+	$('.lp-fade-mzsm').show()
+})
+
+
+
+
+$('#mobile').on('input propertychange', function() {
+	var e = $('#mobile').val();
+	if (e.length > 11) {
+		if (!isMobile($('#mobile').val())) {
+			$('.lp-m-tips').show().text('您输入的手机号错误,请重新输入');
+			return !1
+		};
+		$('.lp-m-submit').hide();
+		$('.lp-m-submit2').show()
+	} else if (e.length == 11) {
+		if (!isMobile($('#mobile').val())) {
+			$('.lp-m-tips').show().text('您输入的手机号错误,请重新输入');
+			return !1
+		};
+		$('.lp-m-tips').hide();
+		$('.lp-m-submit').hide();
+		$('.lp-m-submit2').show()
+	} else {
+		$('.lp-m-submit').show();
+		$('.lp-m-submit2').hide();
+		$('.lp-m-tips').hide()
+	}
+});
+
+$('#mobile').on('focus', function(e) {
+	$('body,html').animate({
+		scrollTop: 200
+	}, 500);
+	$('.lp-m-tips').hide()
+});
+
+
+$('.close-btn').click(function() {
+	$('.lp-fade-main').hide()
+	$('.lp-fade-mzsm').hide()
+})
+
+var conciseID = "";
+var codeTF = 0;
+
+$('#emsBtn,#emsBtn2').on('click', function() {
+	$.ajax({
+	 type: 'POST',
+		contentType: 'application/json;charset=UTF-8',
+	 url: 'http://daichabao.100zhi.com/addUserConcise',
+	 data:'{"phone": "'+$.trim($('#mobile').val())+'"}',
+	 dataType:"json",
+	 success: function(data) {
+				conciseID = data.data;
+				var IDlen = conciseID.length;
+				$.ajax({
+				 type: 'POST',
+				 contentType: 'application/json;charset=UTF-8',
+				 url: 'http://daichabao.100zhi.com/addUserDetails',
+				 data:'{"concise_id": "'+conciseID+'"}',
+				 dataType:"json",
+				 success: function(data) {
+						//$('#loading').hide();
+						//clearTimeout(t);
+							if (data.code == 31){
+								var codeTF = 1
+								$('#loading').show();
+								setTimeout(function() {
+									$('#loading').hide();
+									$('.lp-fade-main').hide()
+									window.location.href = "http://daichabao.100zhi.com/info.html?userid="+conciseID;
+								}, 4000);
+							}else if (data.code == 33){
+								$('.lp-fade-cont-tips').show().text("");
+								$('.lp-fade-main').show()
+							}
+						},
+				});
+   			 },
+	 dataType: "json"
+	});
+		
+	//yrdSmsQuery()
+});
+
+
+$('#submitBtn').on('click', function() {
+	if (conciseID == ""){
+		$('.lp-fade-cont-tips').show().text('非法操作');
+		$('.lp-fade-main').hide()
+		return !1
+	}
+	if ($('#namecode').val() == '') {
+		$('.lp-fade-cont-tips').show().text('请输入您的姓名!');
+		return !1
+	};
+	if ($('#idcode').val() == '') {
+		$('.lp-fade-cont-tips').show().text('请输入您的身份证号码!');
+		return !1
+	};
+	if ($('#wxcode').val() == '') {
+		$('.lp-fade-cont-tips').show().text('请输入你的微信或者QQ号码!');
+		return !1
+	};
+	//$('#loading').show();
+	var m = $.trim($('#mobile').val()),
+		n = $.trim($('#namecode').val()),
+		i = $.trim($('#idcode').val()),
+		w = $.trim($('#wxcode').val());
+	$.ajax({
+	 type: 'POST',
+	 contentType: 'application/json;charset=UTF-8',
+	 url: 'http://daichabao.100zhi.com/addUserDetails',
+	 data:'{"concise_id": "'+conciseID+'","name": "'+n+'","card_id": "'+i+'","wx_id": "'+w+'"}',
+	 dataType:"json",
+	 success: function(data) {
+			//$('#loading').hide();
+			//clearTimeout(t);
+			if (data != ""){
+				$('.lp-fade-cont-tips').show().text(data.data);
+				}
+			if(data.code == 18){
+				$('.lp-fade-cont-tips').show().text("");
+				$('#loading').show();
+				$('.lp-fade-main').hide();
+				setTimeout(function() {
+						$('#loading').hide();
+						window.location.href = "http://daichabao.100zhi.com/info.html?userid="+conciseID
+					}, 4000);
+				}
+   			 },
+	 dataType: "json"
+	});
+	
+});
+
+$('#mzsmBtn').click(function() {
+	$('.lp-fade-mzsm').hide()
+});
+
+$('.lp-fixed-close').click(function() {
+	$('.lp-fixed-main').hide()
+	$('.lp-fade-mzsm').hide()
+});
---
+++ code/js/info.js
@@ -0,0 +1,49 @@
+﻿//javascript获取url参数的方法
+(function (jQuery) {
+	$.getUrlParam = function (name) {
+		var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+		var r = window.location.search.substr(1).match(reg);
+		if (r != null) return unescape(r[2]); return null;
+	}
+})(jQuery);
+
+function getLocalTime(nS) {     
+   return new Date(parseInt(nS) * 1).toLocaleString().replace(/:\d{1,2}$/,' ');     
+}
+
+
+
+//info页面去得ID
+var conciseidinfo = jQuery.getUrlParam('userid');
+
+$.ajax({
+	 type: 'POST',
+	 contentType: 'application/json;charset=UTF-8',
+	 url: 'http://daichabao.100zhi.com/selectAmount',
+	 data:'{"concise_id": "'+conciseidinfo+'","operator": "个人查询"}',
+	 success: function(data) {
+				 if (data != ""){
+					 $('#t1').show().html("<strong>报告时间: </strong>"+getLocalTime(data.date)+"<span><strong>编号: </strong>"+data.id+"</span>");
+					 $('#t2').show().html("<strong>姓名: </strong>"+data.name+"<span><strong>身份证: </strong>"+data.card_id+"</span>");
+					 $('#t3').show().html("<strong>年龄: </strong>"+data.age+"<span><strong>性别: </strong>"+data.sex+"</span>");
+				 }else{
+					 alert("该用户信息尚未激活，请验证手机号码")
+					 }
+   			 },
+	 dataType: "json"
+	});
+	
+$.ajax({
+	 type: 'POST',
+	 contentType: 'application/json;charset=UTF-8',
+	 url: 'http://daichabao.100zhi.com/getRecord',
+	 data:'{"concise_id": "'+conciseidinfo+'"}',
+	 success: function(data) {
+				 for( var i = 0; i <= data.data.length; i++ ) {
+					 q=i+1;
+					 $('#record').show().append("<li><i>"+q+"</i>"+data.data[i].operator+"<span>"+getLocalTime(data.data[i].get_time)+"")
+				 }
+   			 },
+	 dataType: "json"
+	});
+	
