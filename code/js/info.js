ee2855c0b6dd7878298c4141165bcbef39c140c7
rescdn <2472066197@qq.com>
2018-04-18 20:06:34 +0800

    6666666

--- code/js/info.js
+++ code/js/info.js
@@ -36,7 +36,7 @@ var amountall= amount.split(";")
 $('#t1').show().html("<strong>报告时间: </strong>"+amountall[0]+"<span><strong>编号: </strong>"+amountall[1]+"</span>");
 $('#t2').show().html("<strong>姓名: </strong>"+amountall[2]+"<span><strong>身份证: </strong>"+amountall[3]+"</span>");
 $('#t3').show().html("<strong>年龄: </strong>"+amountall[4]+"<span><strong>性别: </strong>"+amountall[5]+"</span>");
-$('.rsshare').show().html("你当前已成功邀请到了 "+amountall[6]+" 位好友")
+$('.rsshare span').show().html(amountall[6])
 
 daidata = "消费贷;6-24期;20000元$$身份证贷;不限;50000元$$社保贷;12-36期;60000元$$保单贷;12-36期;60000元$$公积金贷;12-36期;80000元$$微粒贷;6-24期;100000元$$淘宝贷;6-24期;40000元$$京东贷;6-24期;40000元$$学历贷;6-24期;60000元$$流水贷;6-24期;50000元$$生意贷;12-36期;100000元$$退休贷;12-36期;70000元$$租金贷;6-24期;100000元$$卡友贷;6-24期;50000元$$薪生贷;12-36期;80000元$$飞机贷;6-24期;80000元"
 var daidataall= daidata.split("$$")
