ecfe668f77c4e0d5e67570f160bc8397d3c832d1
rescdn <2472066197@qq.com>
2018-04-18 21:20:56 +0800

    6666666666

--- code/js/info.js
+++ code/js/info.js
@@ -39,6 +39,12 @@ $('#t2').show().html("<strong>姓名: </strong>"+amountall[2]+"<span><strong>身
 $('#t3').show().html("<strong>年龄: </strong>"+amountall[4]+"<span><strong>性别: </strong>"+amountall[5]+"</span>");
 $('.rsshare span').show().html(amountall[6])
 
+if(Number(amountall[4]) <= 23 && amountall[5] == "女"){
+					 $('#u1').show().append("<li class='mn'>"+daidatasplit[0]+"</li>")
+					 $('#u2').show().append("<li class='mn'>"+daidatasplit[1]+"</li>")
+					 $('#u3').show().append("<li class='mn'>"+daidatasplit[2]+"</li>")
+}
+
 
 daidata = "消费贷;6-24期;20000元$$身份证贷;不限;50000元$$社保贷;12-36期;60000元$$保单贷;12-36期;60000元$$公积金贷;12-36期;80000元$$微粒贷;6-24期;100000元$$淘宝贷;6-24期;40000元$$京东贷;6-24期;40000元$$学历贷;6-24期;60000元$$流水贷;6-24期;50000元$$生意贷;12-36期;100000元$$退休贷;12-36期;70000元$$租金贷;6-24期;100000元$$卡友贷;6-24期;50000元$$薪生贷;12-36期;80000元$$飞机贷;6-24期;80000元"
 var daidataall= daidata.split("$$")
@@ -52,7 +58,7 @@ if(amountall[6] > 0){
 					 $('#u3').show().append("<li class=t"+t+">"+daidatasplit[2]+"</li>")
 }
 }else{
-$('.rsshare').show().html("你当前已成功邀请到了 <span>0</span> 位好友 <a href=http://daichabao.100zhi.com/pause.html?m="+phone+">点击刷新数据</a>")	
+$('.rsshare').show().html("你当前已成功邀请到了 <span>0</span> 位好友 <a href=http://daichabao.100zhi.com/pause.html?m="+window.btoa(phone)+">点击刷新数据</a>")	
  for( var i = 0; i < 11; i++ ) {
 					 q=i+1;
 					 t=q+3;
